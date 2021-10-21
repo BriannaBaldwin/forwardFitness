3daa09460865f957c15238fffde6cda4eedd4424
Brianna Baldwin <73656451+BriannaBaldwin@users.noreply.github.com>
2021-10-21 15:53:14 -0600

    Add files via upload

---
+++ forwardFitnessWebsite/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Brianna Baldwin
+    File Name: script.js
+    Date: November 30, 2020
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
